0eaa1f4c7e93a88470abc69b8942ff863a75df00
Risto Vuorio <vuoristo@gmail.com>
2022-04-01 16:35:26 +0000

    Use scan instead of for loops inside switch logic. Make jittable

--- switch_env.py
+++ switch_env.py
@@ -77,7 +77,6 @@ def get_step_env(
     layer_fns:  list[LayerFnType],
 ) -> StepFnType:
     """Get a function for stepping a gridworld environment"""
-
     def step_env_fn(
         rngkey: KeyType,
         in_state: State,
@@ -104,12 +103,12 @@ def agent_fn(
     a_to_move = jnp.array([[0, -1], [1, 0], [0, 1], [-1, 0], [0, 0]])
     layer = grid[layer_index]
     grid_shape = layer.shape
-    locs = jnp.stack(jnp.where(layer == 1), axis=1)
+    loc = jnp.stack(jnp.where(layer == 1, size=1), axis=1)
     shift_fn = lambda l: jnp.clip(l + a_to_move[shift_action],
                        jnp.array((0, 0)), jnp.array(grid_shape) - 1)
-    new_locs = jax.vmap(shift_fn)(locs)
-    new_layer = layer.at[locs[:, 0], locs[:, 1]].set(0.0)
-    new_layer = new_layer.at[new_locs[:, 0], new_locs[:, 1]].set(1.0)
+    new_loc = jax.vmap(shift_fn)(loc)
+    new_layer = layer.at[loc[:, 0], loc[:, 1]].set(0.0)
+    new_layer = new_layer.at[new_loc[:, 0], new_loc[:, 1]].set(1.0)
     new_grid = grid.at[layer_index].set(new_layer)
     new_state = next_state._replace(observation=new_grid)
     return new_state
@@ -147,20 +146,30 @@ def get_switch_fn(
         layer_index: int,
     ) -> State:
         next_obs = next_state.observation
-        agent_loc = jnp.stack(jnp.where(next_obs[agent_layer_index] == 1), axis=1)
-        switch_info = next_state.hidden
-        for _, i_loc, _, _ in switch_info:
-            next_obs = next_obs.at[i_loc[0], i_loc[1], i_loc[2]].set(0.0)
-        reward = 0.0
-        terminal = 0.0
-        for s_loc, i_loc, r, t in switch_info:
+        agent_loc = jnp.stack(jnp.where(next_obs[agent_layer_index] == 1, size=1), axis=1)
+        switch_info = list(map(jnp.array, zip(*next_state.hidden)))
+        def switch_resetter(o, si):
+            i_loc = si[1]
+            o = o.at[i_loc[0], i_loc[1], i_loc[2]].set(0.0)
+            return o, o
+        next_obs, _ = jax.lax.scan(switch_resetter, next_obs, switch_info)
+        def switch_setter(prev_state, si):
+            reward, terminal, o = prev_state
+            s_loc, i_loc, r, t = si
             switch_hit = jnp.all(agent_loc == jnp.array(s_loc[1:]))
-            no_hit_layer = next_obs[i_loc[0]]
+            no_hit_layer = o[i_loc[0]]
             hit_layer = no_hit_layer.at[i_loc[1], i_loc[2]].set(1.0)
             new_indicator_layer = jax.lax.select(switch_hit, hit_layer, no_hit_layer)
-            next_obs = next_obs.at[i_loc[0]].set(new_indicator_layer)
+            o = o.at[i_loc[0]].set(new_indicator_layer)
             reward = reward + jax.lax.select(switch_hit, r, 0.0)
             terminal = terminal + jax.lax.select(switch_hit, t, 0.0)
+            next_state = (reward, terminal, o)
+            return next_state, 0
+        (reward, terminal, next_obs), _ = jax.lax.scan(
+            switch_setter,
+            (0.0, 0.0, next_obs),
+            switch_info,
+        )
         next_state = next_state._replace(observation=next_obs, reward=reward,
                                          terminal=terminal)
         return next_state
